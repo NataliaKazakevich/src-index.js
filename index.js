b82a98d13192e108dc06119d4b0632e44c69e1b9
Natalia Kazakevich <Natallia_Kazakevich@mail.ru>
2018-09-23 21:40:17 +0300

    love-triangle

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function convertTemp(degree) {  
-    if (degree == "C") {   
+function convertTemp(temperature) {  
+/**    if (degree == "C") {   
          F = document.getElementById("c").value * 9 / 5 + 32; 
             document.getElementById("f").value = Math.round(F);
-    }
+    } */
+    ///o_O
+    //just write following OMG))
 }
